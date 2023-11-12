cfdbe775eb69d338f2bc63cc79a1de726bd2bb31
MarioContero <macontero@puce.edu.ec>
2023-11-12 13:20:45 -0500

    features ejemplos

---
+++ prueba.java
@@ -0,0 +1,10 @@
+// archivo: tarea.js
+
+// Agregar una propiedad 'completada' a la tarea
+class Tarea {
+    constructor(descripcion) {
+      this.descripcion = descripcion;
+      this.completada = false; // nueva propiedad
+    }
+  }
+  
